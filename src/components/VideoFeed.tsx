0c9d299884895df23d84430989b4ecacfc244767
Deepanshu Jha <iamdevdeepanshu@gmail.com>
2024-12-14 17:09:26 +0530

    Update VideoFeed.tsx

--- src/components/VideoFeed.tsx
+++ src/components/VideoFeed.tsx
@@ -10,7 +10,7 @@ export function VideoFeed({ isConnected }: VideoFeedProps) {
       <div className="relative">
         {isConnected ? (
           <img 
-            src="https://localhost:5000/video_feed" 
+            src="http://127.0.0.1:5000/video_feed" 
             alt="Video Feed" 
             className="w-full rounded-lg shadow-2xl"
           />
